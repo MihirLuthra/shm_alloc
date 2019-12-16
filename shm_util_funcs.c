c9556bbd70a271620a076c19027c26fece5bfd05
Mihir Luthra <1999mihir.luthra@gmail.com>
2019-12-16 17:35:51 +0530

    fix shm_bitmap_ffs() & (uns|s)et_bit_race_free()

--- shm_util_funcs.c
+++ shm_util_funcs.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 
+#include "cas.h"
 #include "shm_debug.h"
 #include "shm_err.h"
 #include "shm_util_funcs.h"
@@ -33,16 +34,15 @@ unsigned long long get_next_power_of_two(unsigned long long num)
 	return (1ULL << ((sizeof(unsigned long long)*8) - __builtin_clzll(num-1)));
 }
 
-
 int shm_bitmap_ffs(shm_bitmap bmp[BMP_ARR_SIZE])
 {
-	int pos;
+    int pos;
 
-	for (int i = (BMP_ARR_SIZE-1), pos_offt = 0 ; i >= 0 ; --i, pos_offt += BITS)
-		if ((pos = __builtin_ffsll(bmp[i])) != 0)
-				return (BITMAP_SIZE - (pos_offt + (pos-1)));
+    for (int i = (BMP_ARR_SIZE-1) ; i >= 0 ; --i)
+        if ((pos = __builtin_ffsll(bmp[i])) != 0)
+            return (((BMP_ARR_SIZE - 1 - i) * BITS) + pos - (BITS > BITMAP_SIZE ? (BITS-BITMAP_SIZE) :0));
 
-	return 0;
+    return 0;
 }
 
 int get_abs_bit_pos(int rel_bit_pos, size_t mem_level)
@@ -52,7 +52,6 @@ int get_abs_bit_pos(int rel_bit_pos, size_t mem_level)
 
 	return (get_start_bit_pos_for_mem_level(mem_level) + rel_bit_pos);
 }
-
 
 int get_rel_bit_pos(int abs_bit_pos)
 {
@@ -78,32 +77,30 @@ bool set_bit_race_free(_Atomic(shm_bitmap) bmp[BMP_ARR_SIZE], int pos)
 {
 	assert(pos >= 0 && pos < BITMAP_SIZE);
 
-	shm_bitmap old_bmp[BMP_ARR_SIZE], new_bmp[BMP_ARR_SIZE] = {0};
-
-	set_bit(new_bmp, pos);
-
+	shm_bitmap old_bmp, new_bmp, mask;
 	bool cas_result;
+	int index;
 
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i) {
+	index = pos/BITS;
 
-		cas_result = true;
+	mask = 0;
+	mask |= 1ULL << (BITS - (pos % BITS) - 1);
 
-		do {
-			old_bmp[i] = bmp[i];
-			new_bmp[i] |= old_bmp[i];
+	assert(mask != 0);
 
-			if (old_bmp[i] == new_bmp[i]) {
+	cas_result = true;
 
-				cas_result = false;
-				break;
-			}
+	do {
+		old_bmp = bmp[index];
+		new_bmp = mask | old_bmp;
 
-		} while ( !atomic_compare_exchange_strong_explicit(&bmp[i], &old_bmp[i], new_bmp[i], memory_order_relaxed, memory_order_relaxed) );
+		if (old_bmp == new_bmp) {
 
-
-		if (cas_result == true)
+			cas_result = false;
 			break;
-	}
+		}
+
+	} while ( !CAS(&bmp[index], &old_bmp, new_bmp) );
 
 	return (cas_result);
 }
@@ -112,31 +109,30 @@ bool unset_bit_race_free(_Atomic(shm_bitmap) bmp[BMP_ARR_SIZE], int pos)
 {
 	assert(pos >= 0 && pos < BITMAP_SIZE);
 
-	shm_bitmap old_bmp[BMP_ARR_SIZE], new_bmp[BMP_ARR_SIZE] = {0};
-
-	set_bit(new_bmp, pos);
-
+	shm_bitmap old_bmp, new_bmp, mask;
 	bool cas_result;
+	int index;
 
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i) {
+	index = pos/BITS;
 
-		cas_result = true;
+	mask = 0;
+	mask |= 1ULL << (BITS - (pos % BITS) - 1);
 
-		do {
-			old_bmp[i] = bmp[i];
-			new_bmp[i] = old_bmp[i] & ~(new_bmp[i]);
+	assert(mask != 0);
 
-			if (old_bmp[i] == new_bmp[i]) {
+	cas_result = true;
 
-				cas_result = false;
-				break;
-			}
+	do {
+		old_bmp = bmp[index];
+		new_bmp = ~(mask) & old_bmp;
 
-		} while ( !atomic_compare_exchange_strong_explicit(&bmp[i], &old_bmp[i], new_bmp[i], memory_order_relaxed, memory_order_relaxed) );
+		if (old_bmp == new_bmp) {
 
-		if (cas_result == true)
+			cas_result = false;
 			break;
-	}
+		}
+
+	} while ( !CAS(&bmp[index], &old_bmp, new_bmp) );
 
 	return (cas_result);
 }
@@ -148,6 +144,16 @@ void unset_bit(shm_bitmap bmp[BMP_ARR_SIZE], int pos)
 	bmp[idx] &= ~((shm_bitmap)1 << (BITS - (pos % BITS) - 1));
 }
 
+void get_settable_bits_mask(shm_bitmap bmp[BMP_ARR_SIZE], size_t mem, shm_bitmap mask[BMP_ARR_SIZE])
+{
+    set_mask_for_range(mask, get_start_bit_pos_for_mem_level(mem), get_end_bit_pos_for_mem_level(mem));
+
+	int i;
+
+    for (i = 0 ; i < BMP_ARR_SIZE ; ++i) {
+        mask[i] = mask[i] & ~bmp[i];
+	}
+}
 
 void set_mask_for_range(shm_bitmap mask[BMP_ARR_SIZE], int from, int to)
 {
@@ -164,30 +170,6 @@ void set_mask_for_range(shm_bitmap mask[BMP_ARR_SIZE], int from, int to)
 
 	mask[end_idx]    = mask[end_idx] << ( BITS - ((to-1) % BITS) - 1);
 	mask[start_idx] &= (shm_bitmap)-1 >> (from % BITS);
-}
-
-void shm_bitmap_and(shm_bitmap bmp1[BMP_ARR_SIZE], shm_bitmap bmp2[BMP_ARR_SIZE], shm_bitmap res[BMP_ARR_SIZE])
-{
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i)
-		res[i] = bmp1[i] & bmp2[i];
-}
-
-void shm_bitmap_nand(shm_bitmap bmp1[BMP_ARR_SIZE], shm_bitmap bmp2[BMP_ARR_SIZE], shm_bitmap res[BMP_ARR_SIZE])
-{
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i)
-		res[i] = ~(bmp1[i] & bmp2[i]);
-}
-
-void shm_bitmap_or(shm_bitmap bmp1[BMP_ARR_SIZE], shm_bitmap bmp2[BMP_ARR_SIZE], shm_bitmap res[BMP_ARR_SIZE])
-{
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i)
-		res[i] = bmp1[i] | bmp2[i];
-}
-
-void shm_bitmap_not(shm_bitmap bmp[BMP_ARR_SIZE], shm_bitmap res[BMP_ARR_SIZE])
-{
-	for (int i = 0 ; i < BMP_ARR_SIZE ; ++i)
-        res[i] = ~bmp[i];
 }
 
 bool is_bit_range_zero(shm_bitmap bmp[BMP_ARR_SIZE], int from, int to)
