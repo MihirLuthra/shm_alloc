50ba06b3fe4c28c5559043e7d6f5eee39e6a0af4
Mihir Luthra <1999mihir.luthra@gmail.com>
2019-12-16 17:27:15 +0530

    add get_shm_m(ax|in)_allocatable_size() and shm_init()

--- shm_alloc.h
+++ shm_alloc.h
@@ -3,15 +3,15 @@
 
 #include "shm_user_types.h"
 
+void __attribute__((constructor)) shm_init(void);
+
 void * get_shm_user_base(void);
 
 shm_offt shm_malloc(size_t size);
 shm_offt shm_calloc(size_t count, size_t size);
 void     shm_free(shm_offt shm_ptr);
 
-size_t get_max_allocatable_shm_size();
-size_t get_max_possible_offset();
-size_t get_user_max_block_size();
-size_t get_user_min_block_size();
+size_t get_shm_max_allocatable_size();
+size_t get_shm_min_allocatable_size();
 
 #endif /* !defined(__SHM_ALLOC_H__) */
