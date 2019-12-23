2e153e5e0d6f9b78ae9af8a18a5fda6ee0ebac29
Mihir Luthra <1999mihir.luthra@gmail.com>
2019-12-23 14:03:47 +0530

    add getter for shm data table regiom

--- shm_constants.c
+++ shm_constants.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 /*
- * Access offset to shm null via 
+ * Access offset to shm null via
  * get_shm_null_base_offt() and size of shm null by
  * get_shm_null_size()
  *
@@ -25,7 +25,7 @@ static struct {
 void shm_null_init()
 {
     /* shm null comes after MGMT area */
-    shm_null.offt = get_shm_mgmt_base_offt() + SHM_MGMT_SIZE;
+    shm_null.offt = get_shm_mgmt_base_offt() + get_shm_mgmt_size();
 
     /* align it to page boundary */
     shm_null.offt += DIFF_NEXT_PAGE_BOUNDARY(shm_null.offt);
@@ -33,28 +33,33 @@ void shm_null_init()
     shm_null.size = getpagesize();
 }
 
-const size_t get_shm_mapping_size()
+size_t get_shm_mapping_size()
 {
     assert(shm_null.size != -1);
 
     /*
      * mapping comprises of 3 parts:
      *
-     * (1)
+	 * (1)
+	 *     shm data table
+	 *
+     * (2)
      *     management area of size SHM_MGMT_SIZE
      *
-     * (2)
-     *     shm null of pagesize
-     * 
      * (3)
+     *     shm null of pagesize
+     *
+     * (4)
      *     allocatable area of shm of size MAX_ALLOCATABLE_SHM_SIZE
 	 *
 	 * NOTE:
 	 *  The base returned to the user starts from shm null offset
      */
-    size_t mapping_size;
+    size_t mapping_size = 0;
 
-    mapping_size = SHM_MGMT_SIZE;
+	mapping_size += get_shm_data_table_size();
+    mapping_size = get_shm_mgmt_size();
+
     mapping_size += DIFF_NEXT_PAGE_BOUNDARY(mapping_size);
 
     mapping_size += shm_null.size;
@@ -63,33 +68,48 @@ const size_t get_shm_mapping_size()
     return (mapping_size);
 }
 
-const shm_offt get_shm_mgmt_base_offt()
+shm_offt get_shm_data_table_offt()
 {
 	return (0);
 }
 
-const shm_offt get_shm_null_base_offt()
+shm_offt get_shm_data_table_size()
+{
+	return (sizeof(struct shm_data_table));
+}
+
+shm_offt get_shm_mgmt_base_offt()
+{
+	return (get_shm_data_table_offt() + get_shm_data_table_size());
+}
+
+shm_offt get_shm_mgmt_size()
+{
+	return (SHM_MGMT_SIZE);
+}
+
+shm_offt get_shm_null_base_offt()
 {
 	assert(shm_null.size != -1);
 	return (shm_null.offt);
 }
 
-const shm_offt get_shm_user_base_offt()
+shm_offt get_shm_user_base_offt()
 {
 	assert(shm_null.size != -1);
 	return (shm_null.offt);
 }
 
-const size_t get_shm_null_size()
+size_t get_shm_null_size()
 {
 	assert(shm_null.size != -1);
 	return (shm_null.size);
 }
 
-const shm_offt get_allocatable_shm_base_offt()
+shm_offt get_allocatable_shm_base_offt()
 {
 	assert(shm_null.size != -1);
-	
+
 	shm_offt ret_offt = get_shm_null_base_offt() + get_shm_null_size();
 
 	return (ret_offt);
