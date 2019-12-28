bc569d423c83c230593c045ea84682a9414572e1
Mihir Luthra <1999mihir.luthra@gmail.com>
2019-12-28 16:54:35 +0530

    use lfstack submodule as OSAtomicQueue is mac specific

--- tests/test_shm_alloc.c
+++ tests/test_shm_alloc.c
@@ -1,12 +1,20 @@
-#include <assert.h>
-
-#include "shm_alloc.h"
-
 #include <errno.h>
-#include <libkern/OSAtomicQueue.h>
+
+#if defined __has_include
+#	if __has_include (<libkern/OSAtomicQueue.h>)
+#		include <libkern/OSAtomicQueue.h>
+#	define HAVE_OSATOMICQUEUE
+#	endif
+#endif
+
+#if !defined(HAVE_OSATOMICQUEUE)
+#	include "lstack.h"
+#endif
+
 #include <pthread.h>
 
 #include "rand_string_generator.h"
+#include "shm_alloc.h"
 
 #include <stdatomic.h>
 #include <stdbool.h>
@@ -46,13 +54,19 @@ char **rand_strings;
 long max_idx;
 _Atomic(long) cur_idx = 0;
 
+#if defined(HAVE_OSATOMICQUEUE)
 OSFifoQueueHead queue = OS_ATOMIC_FIFO_QUEUE_INIT;
+#else
+lstack_t stack;
+#endif
 
 struct inserted_data_mgr {
 	PTR(char) in_shm;
 	int       idx;
 
+#if defined(HAVE_OSATOMICQUEUE)
 	struct inserted_data_mgr * link;
+#endif
 };
 
 struct test_results_mgr {
@@ -106,6 +120,13 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+#if !defined(HAVE_OSATOMICQUEUE)
+	if (lstack_init(&stack, max_idx) != 0) {
+		P_ERR("lstack_init() failed");
+		exit(EXIT_FAILURE);
+	}
+#endif
+
 	generate_processes(process_count);
 
 	rand_strings =
@@ -120,6 +141,10 @@ int main(int argc, char *argv[])
 	test_results = spawn_threads_for_test(thread_count, tester_func);
 
 	display_test_results(test_results, thread_count);
+
+#if !defined(HAVE_OSATOMICQUEUE)
+	lstack_free(&stack);
+#endif
 
 	free_rand_strings(rand_strings, max_idx);
 	shm_deinit();
@@ -181,7 +206,7 @@ void *tester_func(void *arg)
 {
 	struct test_results_mgr * test_result = arg;
 
-	struct inserted_data_mgr *inserted_data, *dequed_data;
+	struct inserted_data_mgr *data;
 
 	PTR(char) str;
 	int idx;
@@ -199,28 +224,46 @@ void *tester_func(void *arg)
 
 		strcpy(ACCESS(str, char), rand_strings[idx]);
 
-		inserted_data = malloc(sizeof(struct inserted_data_mgr));
+		data = malloc(sizeof(struct inserted_data_mgr));
 
-		if (inserted_data == NULL) {
+		if (data == NULL) {
 			P_ERR("malloc(2) failed");
 			exit(EXIT_FAILURE);
 		}
 
-		inserted_data->idx      = idx;
-		inserted_data->in_shm   = str;
+		data->idx    = idx;
+		data->in_shm = str;
 
-		OSAtomicFifoEnqueue(&queue, inserted_data, offsetof(struct inserted_data_mgr, link));
+#if defined(HAVE_OSATOMICQUEUE)
+		OSAtomicFifoEnqueue(&queue, data, offsetof(struct inserted_data_mgr, link));
+#else
+		if (lstack_push(&stack, data) != 0) {
+			P_ERR("lstack_push(2) failed");
+			exit(EXIT_FAILURE);
+		}
+#endif
 
 		sleep(0);
 
-		dequed_data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+#if defined(HAVE_OSATOMICQUEUE)
+		data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+#else
+		data = lstack_pop(&stack);
+#endif
+		/*
+		 * The data being popped/dequed maybe the same that was pushed/enqued
+		 * or different if thread got changed. In any case, even if the same data
+		 * is got back, below data is checked for correctness and then if
+		 * a randomly generated number is divisible by 3, only then this data is freed,
+		 * otherwise its pushed/enqued again for getting checked again.
+		 */
 
-		if (dequed_data == NULL) {
+		if (data == NULL) {
 			continue;
 		}
 
-		idx = dequed_data->idx;
-		str = dequed_data->in_shm;
+		idx = data->idx;
+		str = data->in_shm;
 
 		if (strcmp(ACCESS(str, char) , rand_strings[idx])) {
 
@@ -231,40 +274,49 @@ void *tester_func(void *arg)
 
 			/*
 			 * Just to make it more random,
-			 * generate a random number and if its divisible by 2,
+			 * generate a random number and if its divisible by 3,
 			 * shm_free() the dequed offset else enqueue it again
 			 */
 
 			if (rand() % 3 == 0) {
 				shm_free(str);
-				free(dequed_data);
+				free(data);
 			} else {
-				OSAtomicFifoEnqueue(&queue, dequed_data, offsetof(struct inserted_data_mgr, link));
-
+#if defined(HAVE_OSATOMICQUEUE)
+				OSAtomicFifoEnqueue(&queue, data, offsetof(struct inserted_data_mgr, link));
+#else
+				lstack_push(&stack, data);
+#endif
 			}
 		}
 
 	}
 
+#if defined(HAVE_OSATOMICQUEUE)
+		data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+#else
+		data = lstack_pop(&stack);
+#endif
 
-	dequed_data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+	while(data != NULL) {
 
-	while(dequed_data != NULL) {
+		idx = data->idx;
+		str = data->in_shm;
 
-		idx = dequed_data->idx;
-		str = dequed_data->in_shm;
-
-		if (strcmp(ACCESS(str, char) , rand_strings[idx])) {
+		if (strcmp(ACCESS(str, char), rand_strings[idx])) {
 
 			test_result->status = false;
 			break;
 
 		} else {
 			shm_free(str);
-			free(dequed_data);
+			free(data);
 		}
-
-		dequed_data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+#if defined(HAVE_OSATOMICQUEUE)
+		data = OSAtomicFifoDequeue(&queue, offsetof(struct inserted_data_mgr, link));
+#else
+		data = lstack_pop(&stack);
+#endif
 	}
 
 	test_result->tid = (long)pthread_self();
