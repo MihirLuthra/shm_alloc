e24f51e4edd7e462b64165ddbc5f2adbf6771728
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-01-26 16:37:23 +0530

    shm_offt should be always atomic

--- src/shm_user_types.h
+++ src/shm_user_types.h
@@ -2,9 +2,23 @@
 #define __SHM_USER_TYPES_H__
 
 #include <stdio.h>
+#include <stdatomic.h>
 
-typedef size_t shm_offt;
-#define PRIu_shm_offt "zu"
+#if ATOMIC_LONG_LOCK_FREE == 2
+
+typedef unsigned long shm_offt;
+#define PRIu_shm_offt "lu"
+
+#elif ATOMIC_INT_LOCK_FREE == 2
+
+typedef unsigned int shm_offt;
+#define PRIu_shm_offt "u"
+
+#else /* ATOMIC_INT_LOCK_FREE == 2 */
+
+#	error "Neither ATOMIC_LONG_LOCK_FREE nor ATOMIC_INT_LOCK_FREE is 2, can't proceed"
+
+#endif
 
 #define SHM_NULL ((shm_offt)0)
 
